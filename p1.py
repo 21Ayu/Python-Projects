6d7d6907b3d00f4fb1dbbef8ce37209f8eda45ea
Ayush Kaurav <123882702+21Ayu@users.noreply.github.com>
2024-10-13 20:24:47 +0530

    Add files via upload
    simple pyramid

---
+++ p1.py
@@ -0,0 +1,15 @@
+
+## Python Programs to print  simple Pyramids 
+
+n = int(input("Enter the number "))
+
+
+for i in range(0,n):
+
+    for j in range(0,i+1):
+        print("*",end=" ")
+
+    print()        
+
+
+    
